26da36305f92255c14f711462c70b3eaaf153f89
elicarmona1 <elicarmona1@misena.edu.co>
2021-05-28 20:26:13 -0500

    Ejercicio2realizado

--- EjerciciosActividad1/Pdact 1.py
+++ EjerciciosActividad1/Pdact 1.py
@@ -1,3 +1,9 @@
+'''. Realice un código que permita ingresar el producto, cantidad, valor unitario,
+ ciudad de venta.
+Que calcule el valor total de la venta Utilizando un condicional OR Si la ciudad ingresada
+ es Medellín o Cali, dar un descuento de 5% sobre el valor de la venta, para las demás ciudades
+ no aplica el descuento.
+'''
 producto = (input('ingrese el producto: '))
 cantidad = int(input('ingrese la cantidad: '))
 valunitario = int(input('ingrese el valor unitario: '))
